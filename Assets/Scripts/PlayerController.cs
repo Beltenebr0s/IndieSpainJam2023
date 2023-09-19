d613b99d1f08270d2f2f59ba2aae12aae71699d5
Gary <recondo.126333@e.unavarra.es>
2023-09-19 18:34:45 +0200

    Algunos arreglillos del movimiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public GameObject tierra;
     public float speed = 1;
     public float maxSpeed = 50;
+    public float speedToTierra = 1;
 
     private Rigidbody playerRB;
-    private Vector3 screenBounds;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Mira siempre a la tierra
+        this.transform.LookAt(tierra.transform.position);
+
         // comprueba el movimiento que se quiere hacer
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
@@ -41,13 +44,15 @@ public class PlayerController : MonoBehaviour
 
         // se calcula los limites en los que se puede mover el personaje
         float distanceFromCameraZ = Vector3.Distance(new Vector3(0,0, this.transform.position.z), Camera.main.transform.position);
-        float frustumHeight = 2.0f * distanceFromCameraZ * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float frustumHeight = 2.0f * distanceFromCameraZ * Mathf.Tan(Camera.main.fieldOfView * 0.4f * Mathf.Deg2Rad);
         float frustumWidth = frustumHeight / Camera.main.aspect;
+
+        // se comprueba si se traspasan los limites
         Vector3 limPos = transform.position;
         limPos.x = Mathf.Clamp(limPos.x, -frustumWidth, frustumWidth);
         limPos.y = Mathf.Clamp(limPos.y, -frustumWidth, frustumWidth);
 
-        // se corrige en caso de sobrepasarlos 
+        // se corrige en caso de sobrepasarlos
         if (Mathf.Abs(this.transform.position.x - limPos.x) > 0.1f)
             if ((playerRB.velocity.x > 0 && limPos.x > 0) || (playerRB.velocity.x < 0 && limPos.x < 0))
                 playerRB.velocity = new Vector3(0, playerRB.velocity.y, playerRB.velocity.z);
@@ -55,5 +60,15 @@ public class PlayerController : MonoBehaviour
             if ((playerRB.velocity.y > 0 && limPos.y > 0) || (playerRB.velocity.y < 0 && limPos.y < 0))
                 playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
 
+    }
+
+    public void movimientoATierra()
+    {
+        playerRB.velocity = new Vector3(playerRB.velocity.x, playerRB.velocity.y, speedToTierra);
+    }
+
+    public void acelerarMovimientoATierra(float speedAceleration)
+    {
+        playerRB.velocity = new Vector3(playerRB.velocity.x, playerRB.velocity.y, playerRB.velocity.z + speedAceleration);
     }
 }
