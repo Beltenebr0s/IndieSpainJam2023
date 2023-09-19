87d8eb944d3cc08fb64e21d11202b0c02e9df6f7
Gary <recondo.126333@e.unavarra.es>
2023-09-19 18:58:22 +0200

    Lanzamiento y movimiento a tierra

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,12 @@ public class PlayerController : MonoBehaviour
     public GameObject tierra;
     public float speed = 1;
     public float maxSpeed = 50;
-    public float speedToTierra = 1;
+    public float speedLanzamiento = 20f;
+    public float dashSpeed = 10f;
+    public float aceleration = 0.5f;
+
+
+    public bool enCaida = false;
 
     private Rigidbody playerRB;
 
@@ -21,54 +26,73 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Mira siempre a la tierra
-        this.transform.LookAt(tierra.transform.position);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!enCaida)
+                lanzarse();
+            else
+                acelerar(dashSpeed);
+        }
 
-        // comprueba el movimiento que se quiere hacer
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float yMove = Input.GetAxisRaw("Vertical");
+        if (enCaida)
+        {
+            acelerar(aceleration * Time.deltaTime);
 
-        // se añade la aceleracion sin que supere un maximo
-        float normSpeed = 0.01f;
-        if (Mathf.Abs(playerRB.velocity.x + xMove * normSpeed) < maxSpeed)
-            xMove = playerRB.velocity.x + xMove * normSpeed;
-        else
-            xMove = maxSpeed;
-        if (Mathf.Abs(playerRB.velocity.y + yMove * normSpeed) < maxSpeed)
-            yMove = playerRB.velocity.y + yMove * normSpeed;
-        else
-            yMove = maxSpeed;
+            // Mira siempre a la tierra
+            this.transform.LookAt(tierra.transform.position);
 
-        // se asigna la aceleracion
-        playerRB.velocity = new Vector3(xMove, yMove, playerRB.velocity.z) * speed;
+            // comprueba el movimiento que se quiere hacer
+            float xMove = Input.GetAxisRaw("Horizontal");
+            float yMove = Input.GetAxisRaw("Vertical");
 
-        // se calcula los limites en los que se puede mover el personaje
-        float distanceFromCameraZ = Vector3.Distance(new Vector3(0,0, this.transform.position.z), Camera.main.transform.position);
-        float frustumHeight = 2.0f * distanceFromCameraZ * Mathf.Tan(Camera.main.fieldOfView * 0.4f * Mathf.Deg2Rad);
-        float frustumWidth = frustumHeight / Camera.main.aspect;
+            // se añade la aceleracion sin que supere un maximo
+            float normSpeed = 0.01f;
+            if (Mathf.Abs(playerRB.velocity.x + xMove * normSpeed) < maxSpeed)
+                xMove = playerRB.velocity.x + xMove * normSpeed;
+            else
+                xMove = maxSpeed;
+            if (Mathf.Abs(playerRB.velocity.y + yMove * normSpeed) < maxSpeed)
+                yMove = playerRB.velocity.y + yMove * normSpeed;
+            else
+                yMove = maxSpeed;
 
-        // se comprueba si se traspasan los limites
-        Vector3 limPos = transform.position;
-        limPos.x = Mathf.Clamp(limPos.x, -frustumWidth, frustumWidth);
-        limPos.y = Mathf.Clamp(limPos.y, -frustumWidth, frustumWidth);
+            // se asigna la aceleracion
+            playerRB.velocity = new Vector3(xMove, yMove, playerRB.velocity.z) * speed;
 
-        // se corrige en caso de sobrepasarlos
-        if (Mathf.Abs(this.transform.position.x - limPos.x) > 0.1f)
-            if ((playerRB.velocity.x > 0 && limPos.x > 0) || (playerRB.velocity.x < 0 && limPos.x < 0))
-                playerRB.velocity = new Vector3(0, playerRB.velocity.y, playerRB.velocity.z);
-        if (Mathf.Abs(this.transform.position.y - limPos.y) > 0.1f)
-            if ((playerRB.velocity.y > 0 && limPos.y > 0) || (playerRB.velocity.y < 0 && limPos.y < 0))
-                playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
+            // se calcula los limites en los que se puede mover el personaje
+            float distanceFromCameraZ = Vector3.Distance(new Vector3(0,0, this.transform.position.z), Camera.main.transform.position);
+            float frustumHeight = 2.0f * distanceFromCameraZ * Mathf.Tan(Camera.main.fieldOfView * 0.4f * Mathf.Deg2Rad);
+            float frustumWidth = frustumHeight / Camera.main.aspect;
 
+            // se comprueba si se traspasan los limites
+            Vector3 limPos = transform.position;
+            limPos.x = Mathf.Clamp(limPos.x, -frustumWidth, frustumWidth);
+            limPos.y = Mathf.Clamp(limPos.y, -frustumWidth, frustumWidth);
+
+            // se corrige en caso de sobrepasarlos
+            if (Mathf.Abs(this.transform.position.x - limPos.x) > 0.1f)
+                if ((playerRB.velocity.x > 0 && limPos.x > 0) || (playerRB.velocity.x < 0 && limPos.x < 0))
+                    playerRB.velocity = new Vector3(0, playerRB.velocity.y, playerRB.velocity.z);
+            if (Mathf.Abs(this.transform.position.y - limPos.y) > 0.1f)
+                if ((playerRB.velocity.y > 0 && limPos.y > 0) || (playerRB.velocity.y < 0 && limPos.y < 0))
+                    playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
+
+        }
     }
 
-    public void movimientoATierra()
+    public void lanzarse()
     {
-        playerRB.velocity = new Vector3(playerRB.velocity.x, playerRB.velocity.y, speedToTierra);
+        enCaida = true;
+        playerRB.velocity = new Vector3(playerRB.velocity.x, playerRB.velocity.y, speedLanzamiento);
     }
 
-    public void acelerarMovimientoATierra(float speedAceleration)
+    public void acelerar(float speedAceleration)
     {
         playerRB.velocity = new Vector3(playerRB.velocity.x, playerRB.velocity.y, playerRB.velocity.z + speedAceleration);
+    }
+
+    public void golpe(float fuerzaImpacto)
+    {
+        acelerar(-fuerzaImpacto);
     }
 }
