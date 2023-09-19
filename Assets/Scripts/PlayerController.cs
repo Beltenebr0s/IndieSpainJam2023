7cce6b0f646bd1aa0701982e07f13b5ee94ac468
Gary <recondo.126333@e.unavarra.es>
2023-09-19 17:10:02 +0200

    Limitacion de movimiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,49 +14,46 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerRB = this.GetComponent<Rigidbody>();
-        
-
-        float bounds;
-        if (Screen.width <= Screen.height)
-            bounds = Screen.width;
-        else
-            bounds = Screen.height;
-        screenBounds = Camera.main.ViewportToWorldPoint(new Vector3(bounds, bounds));
+        playerRB = this.GetComponent<Rigidbody>();        
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+        // comprueba el movimiento que se quiere hacer
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
 
+        // se añade la aceleracion sin que supere un maximo
         float normSpeed = 0.01f;
-
         if (Mathf.Abs(playerRB.velocity.x + xMove * normSpeed) < maxSpeed)
             xMove = playerRB.velocity.x + xMove * normSpeed;
         else
             xMove = maxSpeed;
-
         if (Mathf.Abs(playerRB.velocity.y + yMove * normSpeed) < maxSpeed)
             yMove = playerRB.velocity.y + yMove * normSpeed;
         else
             yMove = maxSpeed;
 
+        // se asigna la aceleracion
         playerRB.velocity = new Vector3(xMove, yMove, playerRB.velocity.z) * speed;
 
+        // se calcula los limites en los que se puede mover el personaje
+        float distanceFromCameraZ = Vector3.Distance(new Vector3(0,0, this.transform.position.z), Camera.main.transform.position);
+        float frustumHeight = 2.0f * distanceFromCameraZ * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float frustumWidth = frustumHeight / Camera.main.aspect;
+        Vector3 limPos = transform.position;
+        limPos.x = Mathf.Clamp(limPos.x, -frustumWidth, frustumWidth);
+        limPos.y = Mathf.Clamp(limPos.y, -frustumWidth, frustumWidth);
 
-        Vector3 pos = Camera.main.WorldToViewportPoint(this.transform.position);
-        pos.x = Mathf.Clamp01(pos.x);
-        pos.y = Mathf.Clamp01(pos.y);
-        Debug.DrawLine(Camera.main.ViewportToWorldPoint(pos), new Vector3(0,0,-28.17f),Color.red);
-        Vector3 limPos = Camera.main.ViewportToWorldPoint(pos);
-        
+        // se corrige en caso de sobrepasarlos 
         if (Mathf.Abs(this.transform.position.x - limPos.x) > 0.1f)
             if ((playerRB.velocity.x > 0 && limPos.x > 0) || (playerRB.velocity.x < 0 && limPos.x < 0))
                 playerRB.velocity = new Vector3(0, playerRB.velocity.y, playerRB.velocity.z);
         if (Mathf.Abs(this.transform.position.y - limPos.y) > 0.1f)
             if ((playerRB.velocity.y > 0 && limPos.y > 0) || (playerRB.velocity.y < 0 && limPos.y < 0))
                 playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
+
     }
 }
