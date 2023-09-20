5a4f41525dc63291edb983cac2bc8f8ef87e2845
Bea <beatriz.lagoperez102@gmail.com>
2023-09-20 19:37:42 +0200

    Movimiento del misil y el rayo láser.

--- Assets/Scripts/Obstacles/Misile.cs
+++ Assets/Scripts/Obstacles/Misile.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Misile : MonoBehaviour
+public class Misile : MonoBehaviour, IMovingObstacle
 {
-    // Start is called before the first frame update
+    public GameObject earth;
+    public GameObject player;
+    public float force;
+    public Vector3 direction;
+    public Rigidbody rb;
     void Start()
     {
-        
+        earth = GameObject.Find("Earth");
+        player = GameObject.Find("Player");
+        rb = this.GetComponent<Rigidbody>();
+        Throw();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    public void Throw()
+    {
+        Vector3 randomPointInSphere = Random.insideUnitSphere;
+        // Asegurarse de que está entre la Tierra y la Luna para que no
+        // se lance hacia atrás
+        if (randomPointInSphere.z < earth.transform.position.z)
+        {
+            randomPointInSphere.z = -randomPointInSphere.z;
+        }
+        direction = randomPointInSphere - earth.transform.position;
+        rb.AddForce(force * direction.normalized, ForceMode.Impulse);
+    }
+    private void Move()
+    {
+        Vector3 directionToPlayer = (this.transform.position - player.transform.position);
+        // Forma vectorial de la ecuación de gravitación universal para dirigir un poco el misil
+        // hacia la luna :')
+        Vector3 fakeGravityForce = (- (rb.mass * player.GetComponent<Rigidbody>().mass) / directionToPlayer.magnitude)
+                                 * directionToPlayer.normalized;
+        rb.AddForce(fakeGravityForce, ForceMode.Force);
+        Debug.DrawLine(this.transform.position, fakeGravityForce);
+    }
+    public void DamagePlayer(int damageValue){}
+
+    public Vector3 FindStartingPosition()
+    {
+        return earth.transform.position + Vector3.forward * 4f;
+    }
+    
 }
