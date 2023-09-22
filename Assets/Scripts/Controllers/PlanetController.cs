b2d77ab53c782bd32718b390f3a0ad7081e0cd90
Gary <recondo.126333@e.unavarra.es>
2023-09-22 03:43:30 +0200

    He cambaido alguna cosilla de los misiles pero nada realmente

--- Assets/Scripts/Controllers/PlanetController.cs
+++ Assets/Scripts/Controllers/PlanetController.cs
@@ -7,53 +7,23 @@ public class PlanetController : MonoBehaviour
     private bool startGame = false;
     private bool endGame = false;
 
-    /*
-    [Tooltip("Defines the current tracking origin type.")]
-    [InspectorName("Quest 1")]
-    [RangeAttribute(0.5f, 2.0f)]
-    */
-
     public GameController gameController;
-
-    [Header("Atack Settings")]
-    [SerializeField]
-    private float minTimeBetweenAttacks = 2;
-    [SerializeField]
-    private float maxTimeBetweenAttacks = 5;
-    private float timeSinceLastAttack = 0f;
-    private float timeToNextAttack = 0f;
-    [SerializeField]
-    private List<GameObject> weaponsList;
 
 
     private void Start()
     {
-        timeToNextAttack = Random.Range(minTimeBetweenAttacks, maxTimeBetweenAttacks);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (startGame)
-        {
-            timeSinceLastAttack += Time.deltaTime;
-            if(timeSinceLastAttack >= timeToNextAttack)
-            {
-                //attack();
-            }
-        }
+
     }
 
     public void StartGame()
     {
         startGame = true;
-    }
-
-    private void attack()
-    {
-        timeSinceLastAttack = 0;
-        timeToNextAttack = Random.Range(minTimeBetweenAttacks, maxTimeBetweenAttacks);
-        Instantiate(weaponsList[Random.Range(0, weaponsList.Count)]);
     }
 
     public void EndGame()
---
+++ Assets/Scripts/MObstacles/MSatellite.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MSatellite : MonoBehaviour, IIdleObstacle
+{
+    private GameObject gameController;
+    public GameObject earth;
+    public GameObject player;
+
+    public Vector3 direction;
+
+    public float minSpeed = 30f;
+    public float maxSpeed = 70f;
+    private float speed;
+    private float minDistanceToEarth;
+
+    public float minSize = 0.1f;
+    public float maxSize = 0.7f;
+    
+    private bool near = false;
+
+    void Start()
+    {
+        gameController = GameObject.Find("GameController");
+    }
+
+    void Update()
+    {
+        this.Move();
+
+        if(Vector3.Distance(this.transform.position, player.transform.position) < 10f && this.transform.position.z > player.transform.position.z)
+        {
+            this.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("Satellite near");
+            // -Debug.Break();
+            near = true;
+        }
+        else if(near)
+        {
+            this.GetComponent<Renderer>().material.color = Color.white;
+            near = false;
+        }
+    }
+
+    public void Move()
+    {
+        this.transform.LookAt(earth.transform);
+        this.transform.RotateAround(earth.transform.position, direction, speed * Time.deltaTime);
+    }
+
+    public void DamagePlayer(int damageValue)
+    {
+        player.GetComponent<PlayerHealth>().TakeDamage(damageValue);
+        // Audio
+    }
+
+    public Vector3 FindStartingPosition()
+    {
+        earth = GameObject.Find("Earth");
+        player = GameObject.Find("Player");
+        this.minDistanceToEarth = 0.5f;
+
+        float earthToPlayerDistance = (this.earth.transform.position - this.player.transform.position).magnitude;
+        
+        Vector3 pos = this.earth.transform.position + Random.insideUnitSphere * earthToPlayerDistance * 2;
+
+        this.direction = new Vector3(Random.value, Random.value, Random.value).normalized;
+        speed = Random.Range(minSpeed, maxSpeed);
+
+        this.transform.localScale = Vector3.one * Random.Range(minSize, maxSize);
+
+        return pos;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DamagePlayer(1);
+            
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            // Audio
+            Destroy(collision.gameObject);
+        }
+        Destroy(this.gameObject);
+    }
+}
