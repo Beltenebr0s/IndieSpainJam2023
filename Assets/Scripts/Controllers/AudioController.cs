9c8c92545e8a7f6ec4547fda40764ad617134767
Phoe <dariodebenitopadilla@pm.me>
2023-09-23 02:42:44 +0200

    Más movidas de audio

--- Assets/Scripts/Controllers/AudioController.cs
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -5,20 +5,50 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    private FMOD.Studio.EventInstance instance;
-    public FMODUnity.EventReference gameplayEvent;
+    private FMOD.Studio.EventInstance gameplayMusicI;
+    private FMOD.Studio.EventInstance menuMusicI;
+    private FMOD.Studio.EventInstance deathI;
+    [SerializeField] private FMODUnity.EventReference gameplayMusic;
+    [SerializeField] private FMODUnity.EventReference menuMusic;
+    [SerializeField] private FMODUnity.EventReference moonDeath;
+    [SerializeField] private FMODUnity.EventReference earthDeath;
 
     void Start()
     {
-        instance = FMODUnity.RuntimeManager.CreateInstance(gameplayEvent);
-        instance.start();   
+        menuMusicI = FMODUnity.RuntimeManager.CreateInstance(menuMusic);
+        gameplayMusicI = FMODUnity.RuntimeManager.CreateInstance(gameplayMusic);
+        menuMusicI.start();   
     }
 
     void Update()
     {
         foreach (var field in typeof(AudioParameters).GetFields())
         {
-            instance.setParameterByName(field.Name, (float)field.GetValue(null));
+            gameplayMusicI.setParameterByName(field.Name, (float)field.GetValue(null));
         }
+    }
+
+    public void PlayGameplayMusic()
+    {
+        menuMusicI.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        gameplayMusicI.start();
+    }
+
+    public void PlayMenuMusic() 
+    {
+        gameplayMusicI.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        menuMusicI.start();
+    }
+
+    public void PlayDeath()
+    {
+        deathI = FMODUnity.RuntimeManager.CreateInstance(moonDeath);
+        deathI.start();
+    }
+
+    public void PlayVictory()
+    {
+        deathI = FMODUnity.RuntimeManager.CreateInstance(earthDeath);
+        deathI.start();
     }
 }
